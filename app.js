7adaef578232d1621b5c69177fad4a389f6b4941
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-04-10 17:59:37 +0800

    andythebreaker_testing_init

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ var db = mongoose.connection;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var selectanimalOBJ = require('./routes/selectanimal');
+var pixi_test_router = require('./routes/pixi_test_router');
 
 var app = express();
 
@@ -110,7 +111,7 @@ app.get('*', function (req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/selectanimal', selectanimalOBJ);
-
+app.use('/pixi_text_page', pixi_test_router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
