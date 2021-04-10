23271e2c47a3e916eed5e5c33a85ca21fca9c11a
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-04-10 18:16:44 +0800

    更正資料庫路徑

--- routes/selectanimal.js
+++ routes/selectanimal.js
@@ -6,11 +6,6 @@ router.post('/', ensureAuthenticated, function (req, res, next) {
     res.status(200).json({ "exampleKey": "exampleValue" });
 });
 
-router.get('/pixi', ensureAuthenticated, function (req, res, next) {
-    console.log("=====================");
-  res.render('pixi_test_html', { title: 'pixi_test_page' });
-});
-
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
