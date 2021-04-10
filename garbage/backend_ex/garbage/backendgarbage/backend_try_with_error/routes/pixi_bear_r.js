03c6db80fc4abdd218753f28498cecabe3117a53
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-04-10 16:21:02 +0800

    remove garbage

--- garbage/backend_ex/garbage/backendgarbage/backend_try_with_error/routes/pixi_bear_r.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/pixi_bear2',  function (req, res, next) {
-    res.render('pixi_bear2', { title: 'pixi_bear2' });
-  });
-
-  module.exports = router;
