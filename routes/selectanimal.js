03b2962f83e8a487c583d1bd9727ba28175a13e3
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-04-10 18:42:49 +0800

    更正api回傳

--- routes/selectanimal.js
+++ routes/selectanimal.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', ensureAuthenticated, function (req, res, next) {
-    //console.log("router.post(__/selectanimal__, ensureAuthenticated, function(req, res, next) {");
-    res.status(200).json({ "exampleKey": "exampleValue" });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify([{ message: "Hello World" }, { examplekey: "answer" }]));
+    res.end();
 });
 
 function ensureAuthenticated(req, res, next) {
