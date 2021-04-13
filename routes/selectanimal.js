f68f364c9ed30027755c3e0a347cc49aa20340c1
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-04-13 22:01:46 +0800

    更正json回傳

--- routes/selectanimal.js
+++ routes/selectanimal.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+//app.use(bodyParser.json());
 
 router.post('/', ensureAuthenticated, function (req, res, next) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
