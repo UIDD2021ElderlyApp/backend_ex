2d40a4692b6a2cc39ccfe2dbdf196b61b7d48289
giogggio <55451753+giogggio@users.noreply.github.com>
2021-04-22 17:02:43 +0800

    Add files via upload

---
+++ fb_login/sns/id.js
@@ -0,0 +1,2 @@
+const myAppId = '224423852774215'
+const myClientId = '104741108422999'
---
+++ fb_login/sns/id.sample.js
@@ -0,0 +1,2 @@
+const myAppId = 'put your facebook app id here'
+const myClientId = 'put your google client id here'
