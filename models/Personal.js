dfa30ccd68fc9a3d5c181942023495bb49483043
Ery-z <e24076831@gs.ncku.edu.tw>
2021-06-03 01:52:55 +0800

    嘗試解決資安問題

--- models/Personal.js
+++ models/Personal.js
@@ -35,12 +35,20 @@ module.exports.createPersonal = function (newPersonal, callback) {
 module.exports.setPersonalgetup_time = function (user_name, getup_time, callback) {
     console.log("------->setPersonal");
     var query = { user_name: { $eq: user_name } };
-    Personal.findOneAndUpdate(query, { getup_time: getup_time }, callback)
+    Personal.findOne(query, function(err, Personget){
+        Personget.getup_time = getup_time;
+        Personget.save()
+    })
+    callback()
 }
 module.exports.setPersonalsleep_time = function (user_name, sleep_time, callback) {
     console.log("------->setPersonalsleep_time");
     var query = { user_name: { $eq: user_name } };
-    Personal.findOneAndUpdate(query, { sleep_time: sleep_time }, callback)
+    Personal.findOne(query, function(err, Personget){
+        Personget.sleep_time = sleep_time;
+        Personget.save()
+    })
+    callback()
 }
 
 module.exports.getPersonal = function (user_name, callback) {
