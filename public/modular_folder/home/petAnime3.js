b326824733205489d6fae3d4b1751f22fb6e6d96
littlelian <littlelian5269@gmail.com>
2021-06-24 21:01:51 +0800

    test

--- public/modular_folder/home/petAnime3.js
+++ public/modular_folder/home/petAnime3.js
@@ -19,7 +19,7 @@ function preload ()
 
 function create ()
 {
-    game.renderer.renderSession.roundPixels = true;
+    game.renderer.renderSession.roundPixels = true
     var pet = 'pet3'
     this.player = this.add.sprite(305,300,pet)
     this.player.scaleX = 3
