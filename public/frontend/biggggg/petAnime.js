0e21248c720f997b82e2754185bcce504d3a33d2
littlelian <littlelian5269@gmail.com>
2021-06-25 14:39:47 +0800

    測試動畫模糊問題

--- public/frontend/biggggg/petAnime.js
+++ public/frontend/biggggg/petAnime.js
@@ -16,13 +16,13 @@ var game = new Phaser.Game(config);
 function preload() {
     switch (animal) {
         case 1:
-            this.load.spritesheet('pet', './frontend/biggggg/image/pet1.png', { frameWidth: 209.5, frameHeight: 193 })
+            this.load.spritesheet('pet', './frontend/biggggg/image/pet1.png', { frameWidth: 210, frameHeight: 193 })
             break
         case 2:
-            this.load.spritesheet('pet', './frontend/biggggg/image/pet2.png', { frameWidth: 209.5, frameHeight: 193 })
+            this.load.spritesheet('pet', './frontend/biggggg/image/pet2.png', { frameWidth: 210, frameHeight: 193 })
             break
         case 3:
-            this.load.spritesheet('pet', './frontend/biggggg/image/pet3.png', { frameWidth: 209.5, frameHeight: 193 })
+            this.load.spritesheet('pet', './frontend/biggggg/image/pet3.png', { frameWidth: 210, frameHeight: 193 })
             break
         default:
             console.log("???")
@@ -30,6 +30,7 @@ function preload() {
 }
 
 function create() {
+    game.renderer.renderSession.roundPixels = true
     var pet = 'pet'
     this.player = this.add.sprite(width * 0.33, width * 0.32, pet) // image center vs canvas
     this.player.scaleX = width * 0.0038
