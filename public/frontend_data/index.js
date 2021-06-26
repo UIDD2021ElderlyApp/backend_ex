a3868d478b4c788be19bb3c7dccf5728ae560bcf
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-06-26 12:01:19 +0800

    雷

--- public/frontend_data/index.js
+++ public/frontend_data/index.js
@@ -3,7 +3,7 @@ $(function () {
     if ($(window).width() / $(window).height() > 1.0) {
         console.log('squuuuuuuuuuuuuuuuuuuuuuuuuuuuare');
         document.documentElement.style.setProperty('--var_vw', '(9*(1vh/16))');
-        document.documentElement.style.setProperty('--var_right_global_displacement', '(100vw-var(--var_vw)*100)');
+        document.documentElement.style.setProperty('--var_right_global_displacement', '(100vw - var(--var_vw)*100)');
     }
     init();
 });
