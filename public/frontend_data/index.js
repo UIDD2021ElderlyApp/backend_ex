1d181e5a6bcf162513c4f2a263717f771d999a4f
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-06-26 15:41:25 +0800

    init_main

--- public/frontend_data/index.js
+++ public/frontend_data/index.js
@@ -5,9 +5,9 @@ $(function () {
         document.documentElement.style.setProperty('--var_vw', '(9*(1vh/16))');
         document.documentElement.style.setProperty('--var_right_global_displacement', '(100vw - var(--var_vw)*100)');
     }
-    init();
+    init_main();
 });
-function init() {
+function init_main() {
     document.getElementById('stroll').addEventListener('click', function (e) {
         window.location.href = "/ts/mapview";
     });
