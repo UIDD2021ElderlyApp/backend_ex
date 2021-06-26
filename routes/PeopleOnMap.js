c443ca1f3f9da0c7cb70e23c8df757b46212370b
Ery-z <e24076831@gs.ncku.edu.tw>
2021-06-27 00:29:40 +0800

    地圖"顯示周遭的人"範圍縮小

--- routes/PeopleOnMap.js
+++ routes/PeopleOnMap.js
@@ -135,6 +135,6 @@ function getDistance(lat1, lon1, lat2, lon2, unit) {
         dist = dist * 60 * 1.1515;
         if (unit == "K") { dist = dist * 1.609344 }
         if (unit == "N") { dist = dist * 0.8684 }
-        return dist ;
+        return dist *100;
     }
 }
