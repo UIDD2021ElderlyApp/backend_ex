93aa8b96a25ad0095ec54d632a0d5e3fc4e2ab58
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-06-26 22:58:20 +0800

    console.log("ARARARAR");

--- public/frontend_data/index_layout.js
+++ public/frontend_data/index_layout.js
@@ -7,5 +7,15 @@ function init_layout() {
         console.log('squuuuuuuuuuuuuuuuuuuuuuuuuuuuare');
         document.documentElement.style.setProperty('--var_vw', '(9*(1vh/16))');
         document.documentElement.style.setProperty('--var_right_global_displacement', '(100vw - var(--var_vw)*100)');
+        var is_choose = false;
+        for (let index = 0; index < document.body.classList.length; index++) {
+            if (document.body.classList[index] === "vsc-initialized") {
+                is_choose = true;
+            }
+
+        }
+        if (is_choose) {
+            console.log("ARARARAR");
+        }
     }
 }
