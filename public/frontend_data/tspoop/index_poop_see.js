2868aa9c3da3c66606940c783fbeed3598845ac9
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-06-27 10:44:00 +0800

    document.getElementById("GLOBAL_browse_post_on_scroll").addEventListener("click", function () {     GLOBAL_browse_post_on_scroll = true;scrolledToBottom();

--- public/frontend_data/tspoop/index_poop_see.js
+++ public/frontend_data/tspoop/index_poop_see.js
@@ -24,7 +24,7 @@ GLOBAL_browse_post_on_scroll = true;
 GLOBAL_browse_post_on_scroll_delay_ms = 500;
 
 document.getElementById("GLOBAL_browse_post_on_scroll").addEventListener("click", function () {
-    GLOBAL_browse_post_on_scroll = true;
+    GLOBAL_browse_post_on_scroll = true;scrolledToBottom();
 });
 
 function usr_inp_comment_fcn(e) {
