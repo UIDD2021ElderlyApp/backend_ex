03f1a5fbe0b5a4e9e2f46cb3749f317000b8cef9
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-06-28 01:51:22 +0800

    close buton fucn ok

--- public/frontend_data/index_layout.js
+++ public/frontend_data/index_layout.js
@@ -3,6 +3,7 @@ jQuery_3_6_0(function () {
 });
 function init_layout() {
     document.getElementById('error_msg_gui_group').addEventListener('click', show_err_msg);
+    document.getElementById('error_msg_gui_close').addEventListener('click', disp_err_msg);
     console.log(jQuery_3_6_0(window).width() / jQuery_3_6_0(window).height());
     if (jQuery_3_6_0(window).width() / jQuery_3_6_0(window).height() > 1.0) {
         document.getElementById("is_a_squuuuuuuuuuuuuuare").innerText = "yes";
@@ -53,26 +54,46 @@ function init_layout() {
         }
     }
 }
+
 function show_err_msg(params) {
     console.log("function show_err_msg(params) {");
     var simhtml = document.createElement("div");
     var backcolor = document.createElement("div");
+    var exit_bton_copy = document.createElement("div");
     var pic1 = document.createElement("img");
     var pic2 = document.createElement("img");
-    pic1.src="/frontend_data/img/p1.svg";
-    pic2.src="/frontend_data/img/p2.svg";
+    exit_bton_copy.id = "exit_button_copy";
+    exit_bton_copy.classList.add("show_err_msg_gen");
+    pic1.src = "/frontend_data/img/p1.svg";
+    pic2.src = "/frontend_data/img/p2.svg";
     pic2.classList.add("show_err_msg_gen");
     pic1.classList.add("show_err_msg_gen");
-    pic2.id="show_err_msg_pic2";
-    pic1.id="show_err_msg_pic1";
+    pic2.id = "show_err_msg_pic2";
+    pic1.id = "show_err_msg_pic1";
     simhtml.classList.add("show_err_msg_gen");
-    simhtml.id="show_err_msg_simhtml";
+    simhtml.id = "show_err_msg_simhtml";
     backcolor.classList.add("show_err_msg_gen");
-    backcolor.id="show_err_msg_backcolor";
-    simhtml.style.backgroundColor=document.getElementById("error_msg_gui_blank_part_color").innerText;
+    backcolor.id = "show_err_msg_backcolor";
+    simhtml.style.backgroundColor = document.getElementById("error_msg_gui_blank_part_color").innerText;
     document.getElementById("error_msg_obj").appendChild(simhtml);
     document.getElementById("error_msg_obj").appendChild(backcolor);
     document.getElementById("error_msg_obj").appendChild(pic1);
     document.getElementById("error_msg_obj").appendChild(pic2);
-    document.getElementById("error_msg_obj").style.display="block";
+    document.getElementById("error_msg_obj").appendChild(exit_bton_copy);
+    jQuery_3_6_0("#exit_button_copy").bind('touchstart', function () {
+        jQuery_3_6_0(this).animate({ 'opacity': 0.7 }, 100)
+    });
+    jQuery_3_6_0("#exit_button_copy").bind('touchend', function () {
+        jQuery_3_6_0(this).animate({ 'opacity': 1 }, 100)
+    });
+    document.getElementById("exit_button_copy").addEventListener("click", () => {
+        document.getElementById('error_msg_gui_close').click();
+    });
+    document.getElementById("error_msg_obj").style.display = "block";
+}
+
+function disp_err_msg(){
+    console.log("document.getElementById('error_msg_gui_close').click();");
+    document.getElementById("error_msg_obj").style.display = "none";
+    jQuery_3_6_0('.show_err_msg_gen').remove();
 }
