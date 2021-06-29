0181caf442f498cf88c85d9ec79dfb873f13b3d0
littlelian <littlelian5269@gmail.com>
2021-06-29 18:57:15 +0800

    add wake and sleep function

--- routes/medal.js
+++ routes/medal.js
@@ -69,7 +69,167 @@ router.post('/', ensureAuthenticated, function (req, res) {
     res.status(200).send();
 })
 
+router.post('/setWake', ensureAuthenticated, function(req, res){
+    Person.getPersonal(glob_user_obj.username, function (err, Personget){
+        if (err) throw err;
+        if (Personget) {
+            Medal.getDaily(glob_user_obj.username, function(dailyMissionComplete){
+                var getup = Personget.getup_time[0]*60 + Personget.getup_time[1];
+                var dt = new Date();
+                var nowtime = dt.getHours()*60 + dt.getMinutes();
+                if(Math.abs(nowtime - getup) <= 10 || Math.abs(nowtime - getup) >=1430 && !dailyMissionComplete.wake)
+                {
+                    var input = {
+                        "type": 1,
+                        "goal": 1
+                    };
+                    Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(input), function (db_type) {
+                        if (db_type === -1) {
+                            var newMedal = new Medal({
+                                user_id: glob_user_obj.username,
+                                exp: 0,
+                                wake: false,
+                                sleep: false,
+                                picture: false,
+                                stroll: false,
+                                walk: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                },
+                                post: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                },
+                                message: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                },
+                                level: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                }
+                            });
+                            Medal.createMedal(newMedal, function (err, newMedal) {
+                                if (err) throw err;
+                                console.log(newMedal);
+                            });
+                            Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(input), function (db_type_2) {
+                                console.log("**********Reload to put data**********");
+                                if (db_type_2 === 1) {
+                                    var levelObject = {
+                                        "type": 8,
+                                        "goal": 1
+                                    }
+                                    Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(levelObject), function (db_type_3) {
+                                        console.log("**********Reload to level up**********");
+                                    });
+                                }
+                            }); // reset the data  
+                        }
+                        if (db_type === 1) {
+                            var levelObject = {
+                                "type": 8,
+                                "goal": 1
+                            }
+                            Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(levelObject), function (db_type) {
+                                console.log("**********Reload to level up**********");
+                            });
+                        }
+                    });
+                }
+            })
+        }
+        else {
+            res.status(200).send(-1);
+        }
+    })
+    res.status(200).send();
+})
 
+router.post('/setSleep', ensureAuthenticated, function(req, res){
+    Person.getPersonal(glob_user_obj.username, function (err, Personget){
+        if (err) throw err;
+        if (Personget) {
+            Medal.getDaily(glob_user_obj.username, function(dailyMissionComplete){
+                var sleep = Personget.sleep_time[0]*60 + Personget.sleep_time[1];
+                var dt = new Date();
+                var nowtime = dt.getHours()*60 + dt.getMinutes();
+                if(Math.abs(nowtime - sleep) <= 10 || Math.abs(nowtime - sleep) >=1430 && !dailyMissionComplete.sleep)
+                {
+                    var input = {
+                        "type": 2,
+                        "goal": 1
+                    };
+                    Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(input), function (db_type) {
+                        if (db_type === -1) {
+                            var newMedal = new Medal({
+                                user_id: glob_user_obj.username,
+                                exp: 0,
+                                wake: false,
+                                sleep: false,
+                                picture: false,
+                                stroll: false,
+                                walk: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                },
+                                post: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                },
+                                message: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                },
+                                level: {
+                                    progress: 0,
+                                    finished: false,
+                                    goal: 0
+                                }
+                            });
+                            Medal.createMedal(newMedal, function (err, newMedal) {
+                                if (err) throw err;
+                                console.log(newMedal);
+                            });
+                            Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(input), function (db_type_2) {
+                                console.log("**********Reload to put data**********");
+                                if (db_type_2 === 1) {
+                                    var levelObject = {
+                                        "type": 8,
+                                        "goal": 1
+                                    }
+                                    Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(levelObject), function (db_type_3) {
+                                        console.log("**********Reload to level up**********");
+                                    });
+                                }
+                            }); // reset the data  
+                        }
+                        if (db_type === 1) {
+                            var levelObject = {
+                                "type": 8,
+                                "goal": 1
+                            }
+                            Medal.setMedalByUserId(glob_user_obj.username, JSON.stringify(levelObject), function (db_type) {
+                                console.log("**********Reload to level up**********");
+                            });
+                        }
+                    });
+                }
+            })
+        }
+        else {
+            res.status(200).send(-1);
+        }
+    })
+    res.status(200).send();
+})
 
 router.get('/getdaily', ensureAuthenticated, function (req, res) {
 
