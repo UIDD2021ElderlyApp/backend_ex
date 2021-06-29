0b23d8dda28317fea4e08279f795f8713c8e830d
natsuhane <e24072065@gs.ncku.edu.tw>
2021-06-30 01:42:24 +0800

    back

--- public/modular_folder/login/login.js
+++ public/modular_folder/login/login.js
@@ -2,12 +2,12 @@ jQuery('#status').css({ 'opacity': 0 })
 jQuery('#logout').css({ 'opacity': 0 })
 
 $("#loading_logo").animate({ 'marginTop': '10vh' }, 3000, "easeInOutQuart", function() {
-    $(this).animate({ 'opacity': 0 }, 1000)
-    $("#login_page").animate({ 'opacity': 1 }, 1000)
-})
-$("#login_button").click(function() {
-    $("#waiting_block").show().css('z-index', "10")
-})
+        $(this).animate({ 'opacity': 0 }, 1000)
+        $("#login_page").animate({ 'opacity': 1 }, 1000)
+    })
+    // $("#login_button").click(function() {
+    //     $("#waiting_block").show().css('z-index', "10")
+    // })
 
 $(".button").bind('touchstart', function() {
     $(this).animate({ 'opacity': 0.7 }, 100)
